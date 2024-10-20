2b7782d1dc51b21716c7f4c5e15b2a88d651031a
Dead1405 <159671606+Dead1405@users.noreply.github.com>
2024-10-20 14:46:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
@@ -36,54 +36,77 @@ public class Main {
 
 
 
-class Sets{
-    int[][] sets;
-    Sets(int n){
-        sets = new int[n][2];
-        for(int i = 0; i < n; i++){
-            sets[i][0] = i;
-            sets[i][1] = 1; 
-        }//first part contains parent second contains rank
-    }
+// class Sets{
+//     int[][] sets;
+//     Sets(int n){
+//         sets = new int[n][2];
+//         for(int i = 0; i < n; i++){
+//             sets[i][0] = i;
+//             sets[i][1] = 1; 
+//         }//first part contains parent second contains rank
+//     }
 
-    int findParent(int i){
-        if(sets[i][0] == i) return i;
-        else return findParent(sets[i][0]);
-    }
+//     int findParent(int i){
+//         if(sets[i][0] == i) return i;
+//         else return findParent(sets[i][0]);
+//     }
     
-    void union(int i , int j){
-        i = findParent(i);
-        j = findParent(j);
-        if(sets[i][1] >= sets[j][1]){
-            sets[i][1] += sets[j][1];
-            sets[j][0] = i;
-        }else{
-            sets[j][1] += sets[i][1];
-            sets[i][0] = j;
-        }
-    }
-}
+//     void union(int i , int j){
+//         i = findParent(i);
+//         j = findParent(j);
+//         if(sets[i][1] >= sets[j][1]){
+//             sets[i][1] += sets[j][1];
+//             sets[j][0] = i;
+//         }else{
+//             sets[j][1] += sets[i][1];
+//             sets[i][0] = j;
+//         }
+//     }
+// }
+// class Solution {
+//     static int spanningTree(int V, int E, List<List<int[]>> adj) {
+//         int length = 0;
+//         Sets st = new Sets(V);
+//         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+//         for(int k = 0; k < V; k++){
+//             List<int[]> i = adj.get(k);
+//             for(int[] j : i){
+//                 pq.add(new int[]{k, j[0], j[1]});
+//             }
+//         }
+//         while(!pq.isEmpty()){
+//             int[] i = pq.poll();
+//             if(st.findParent(i[0]) != st.findParent(i[1])){
+//                 length += i[2];
+//                 st.union(i[0], i[1]);
+//             }
+//         }
+//         return length;
+        
+//     }
+// }
 class Solution {
     static int spanningTree(int V, int E, List<List<int[]>> adj) {
-        int length = 0;
-        Sets st = new Sets(V);
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
-        for(int k = 0; k < V; k++){
-            List<int[]> i = adj.get(k);
-            for(int[] j : i){
-                pq.add(new int[]{k, j[0], j[1]});
-            }
-        }
+        int dist = 0;
+        HashSet<Integer> connected = new HashSet<>();
+        //min heap
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1]  - b[1]);
         int count = 0;
+        //add min edge from first vertex
+        for(int[] edge : adj.get(0)) pq.add(edge);
+        connected.add(0);
         while(count < V - 1){
             int[] i = pq.poll();
-            if(st.findParent(i[0]) != st.findParent(i[1])){
-                length += i[2];
-                st.union(i[0], i[1]);
+            if(!connected.contains(i[0])){
+                connected.add(i[0]);
+                dist += i[1];
+                for(int[] j : adj.get(i[0])){
+                    pq.add(j);
+                }
                 count++;
             }
+            
         }
-        return length;
-        
+        return dist;
     }
 }
