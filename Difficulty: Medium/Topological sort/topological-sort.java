442b4cfb28ad9baf1be81d86ee6964d7935d2f3c
Dead1405 <159671606+Dead1405@users.noreply.github.com>
2024-10-27 13:26:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.java
+++ Difficulty: Medium/Topological sort/topological-sort.java
@@ -1,50 +1,49 @@
 //{ Driver Code Starts
-import java.util.*;
 import java.io.*;
 import java.lang.*;
+import java.util.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader read =
-            new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
 
         while (t-- > 0) {
-            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
             String st[] = read.readLine().trim().split("\\s+");
-            int edg = Integer.parseInt(st[0]);
-            int nov = Integer.parseInt(st[1]);
+            int edges = Integer.parseInt(st[0]);
+            int vertices = Integer.parseInt(st[1]);
 
-            for (int i = 0; i < nov; i++)
-                list.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < vertices; i++) adj.add(i, new ArrayList<Integer>());
 
             int p = 0;
-            for (int i = 1; i <= edg; i++) {
+            for (int i = 1; i <= edges; i++) {
                 String s[] = read.readLine().trim().split("\\s+");
                 int u = Integer.parseInt(s[0]);
                 int v = Integer.parseInt(s[1]);
-                list.get(u).add(v);
+                adj.get(u).add(v);
             }
 
-            int[] res = new Solution().topoSort(nov, list);
+            ArrayList<Integer> res = new Solution().topologicalSort(adj);
 
-            if (check(list, nov, res) == true)
+            if (check(adj, vertices, res) == true)
                 System.out.println("1");
             else
                 System.out.println("0");
         }
     }
-    static boolean check(ArrayList<ArrayList<Integer>> list, int V, int[] res) {
-        
-        if(V!=res.length)
-        return false;
-        
+
+    static boolean check(ArrayList<ArrayList<Integer>> adj, int V,
+                         ArrayList<Integer> res) {
+
+        if (V != res.size()) return false;
+
         int[] map = new int[V];
         for (int i = 0; i < V; i++) {
-            map[res[i]] = i;
+            map[res.get(i)] = i;
         }
         for (int i = 0; i < V; i++) {
-            for (int v : list.get(i)) {
+            for (int v : adj.get(i)) {
                 if (map[i] > map[v]) return false;
             }
         }
@@ -55,32 +54,31 @@ class Main {
 // } Driver Code Ends
 
 
-/*Complete the function below*/
-
-
-class Solution
-{
-    static void DFS(ArrayList<ArrayList<Integer>> adj, boolean[] visited, Stack<Integer> stck, int node){
-        visited[node] = true;
+class Solution {
+    // Function to return list containing vertices in Topological order.
+    static void dfs(ArrayList<ArrayList<Integer>> adj, int node, int[] visited, Stack<Integer> stack){
         for(int i : adj.get(node)){
-            if( visited[i] == false) {
-                DFS(adj, visited, stck, i);
+            if ( visited[i] == 0){
+                visited[i] = 1;
+                dfs(adj, i, visited, stack);
+                visited[i] = 2;
             }
         }
-        stck.push(node);
-        
+        stack.push(node);
     }
-    //Function to return list containing vertices in Topological order. 
-    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
-    {
-        boolean[] visited= new boolean[V];
-        Stack<Integer> stck = new Stack<>();
-        int[] ans = new int[V];
-        for(int i = 0; i < V; i++){
-            if(visited[i] != true) DFS(adj, visited, stck, i);
+    static ArrayList<Integer> topologicalSort(ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        int[] visited = new int[adj.size()];
+        for(int i = 0; i < visited.length; i++){
+            if(visited[i] == 0){
+                visited[i] = 1;
+                dfs(adj, i, visited, stack);
+                visited[i] = 2;
+            }
         }
-        for(int i = 0; i < V; i++){
-            ans[i] = stck.pop();
+        while(!stack.isEmpty()){
+            ans.add(stack.pop());
         }
         return ans;
     }
