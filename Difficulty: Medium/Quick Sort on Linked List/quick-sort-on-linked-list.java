0649f02deba517fcd03a0d2158362897ed6902a9
Dead1405 <159671606+Dead1405@users.noreply.github.com>
2024-10-29 00:27:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Quick Sort on Linked List/quick-sort-on-linked-list.java
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int key) {
+        data = key;
+        next = null;
+    }
+}
+
+class SortLL {
+    static Node head;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            String str[] = read.readLine().trim().split(" ");
+            int n = str.length;
+            Node head = new Node(Integer.parseInt(str[0]));
+            addToTheLast(head);
+
+            for (int i = 1; i < n; i++) {
+                int a = Integer.parseInt(str[i]);
+                addToTheLast(new Node(a));
+            }
+
+            GfG gfg = new GfG();
+            Node node = gfg.quickSort(head);
+
+            printList(node);
+            System.out.println();
+        
+System.out.println("~");
+}
+    }
+
+    public static void printList(Node head) {
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+    }
+
+    public static void addToTheLast(Node node) {
+        if (head == null) {
+            head = node;
+        } else {
+            Node temp = head;
+            while (temp.next != null) temp = temp.next;
+            temp.next = node;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*node class of the linked list
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+
+}*/
+// you have to complete this function
+class GfG {
+    public static Node quickSort(Node node) {
+        ArrayList<Node> arr = new ArrayList<>();
+        while(node != null){
+            arr.add(node);
+            node = node.next;
+        }
+        arr.sort((a, b) -> a.data - b.data);
+        for(int i = 0; i < arr.size() - 1; i++){
+            arr.get(i).next = arr.get(i+1);
+        }
+        arr.get(arr.size() - 1).next = null;
+        return arr.get(0);
+    }
+}
